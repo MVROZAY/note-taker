64bd9671d05a83fa3e0cd93a23581d60c154da82
MV ROZAY <anonymous070121@gmail.com>
2023-08-16 05:23:08 -0400

    initial push

--- README.md
+++ README.md
@@ -1,1 +1,11 @@
 # note-taker
+
+![Alt text](note.png)
+
+## Description
+
+This is an application called Note Taker that can be used to write and save notes
+
+## Walkthrough video
+
+## Live Link
---
+++ server.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const fs = require('fs');
+const path = require('path');
+const uuid = require('uuid');
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+// Middleware
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+app.use(express.static('public'));
+
+// HTML Routes
+app.get('/notes', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'notes.html'));
+});
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+});
+
+// API Routes
+app.get('/api/notes', (req, res) => {
+  //const notesData = JSON.parse(fs.readFileSync(path.join(__dirname, 'db.json')));
+  const notesData = fs.readFileSync(path.join(__dirname, 'db.json'));
+  const notes = JSON.parse(notesData);
+  
+  res.json(notesData);
+  res.setHeader('Cache-Control', 'no-cache'); // Disable caching
+  res.json(notes);
+});
+
+app.post('/api/notes', (req, res) => {
+  const newNote = req.body;
+  const notesData = JSON.parse(fs.readFileSync(path.join(__dirname, 'db.json')));
+  newNote.id = uuid.v4(); // Generate a unique ID
+  notesData.push(newNote);
+  fs.writeFileSync(path.join(__dirname, 'db.json'), JSON.stringify(notesData));
+  res.json(newNote);
+});
+
+app.delete('/api/notes/:id', (req, res) => {
+  const notesData = JSON.parse(fs.readFileSync(path.join(__dirname, 'db.json')));
+  const noteId = req.params.id;
+  const updatedNotes = notesData.filter(note => note.id !== noteId);
+  fs.writeFileSync(path.join(__dirname, 'db.json'), JSON.stringify(updatedNotes));
+  res.json({ message: 'Note deleted successfully' });
+});
+
+// Start server
+app.listen(PORT, () => {
+  console.log(`App listening on PORT ${PORT}`);
+});
