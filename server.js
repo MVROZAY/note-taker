24ab12fb13b37287f702ab24b65fea6399a7dd95
MV ROZAY <anonymous070121@gmail.com>
2023-08-18 11:56:11 -0400

    changing

--- server.js
+++ server.js
@@ -46,7 +46,7 @@ app.delete('/api/notes/:id', (req, res) => {
 });
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'public', 'index.html'));
+  res.sendFile(path.join(__dirname, './public/index.html'));
 }); 
 
 // Start server
